7352d372e051a65b3249a1f9adbb322100839de6
HiamSonn <dangthanhson2016@gmail.com>
2023-11-20 21:50:56 +0700

    update SQL

--- QuanLyChiTieu/QuanLyChiTieu/Data/SQLQuery.sql
+++ QuanLyChiTieu/QuanLyChiTieu/Data/SQLQuery.sql
@@ -1,61 +1,62 @@
-﻿create table User_info
+﻿create table NGUOIDUNG
 (
 ID char(6) primary key,
-username varchar(40),
-pass varchar(20),
-phone varchar(20),
-balance money,
+TAIKHOAN varchar(40),
+MATKHAU varchar(20),
+SDT varchar(20),
+TONGTIEN money,
 )
 
-create table NganSach
+create table NGANSACH
 (
-ID char(6) primary key,
-userID char(6),
-NSname varchar(40),
-budget money,
-to_date smalldatetime,
-constraint FK_userID_NganSach foreign key (userID) references User_info(ID),
+MANS char(6) primary key,
+ID char(6),
+TENNS varchar(40),
+TIENNS money,
+HSD smalldatetime,
+constraint FK_ID_NGANSACH foreign key (ID) references NGUOIDUNG(ID),
 )
 
-create table Category
+create table LOAIGIAODICH
 (
-ID char(6) primary key,
-CTname varchar(40),
-GD_status varchar(40),
+MALOAIGD char(6) primary key,
+TENLOAIGD varchar(40),
+TRANGTHAI varchar(40),
 )
 
-create table GiaoDich
+create table GIAODICH
 (
-ID char(6) primary key,
-userID char(6),
-categoryID char(6),
-title varchar(40),
-price money,
-thumbnail text,
-note text,
-created_at smalldatetime,
-constraint FK_userID_GiaoDich foreign key (userID) references User_info(ID),
-constraint FK_categoryID_Category foreign key (categoryID) references Category(ID),
+MAGD char(6) primary key,
+ID char(6),
+MALOAIGD char(6),
+TENGD varchar(40),
+TIEN money,
+MINHHOA text,
+GHICHU text,
+NGAYTAO smalldatetime,
+constraint FK_ID_GIAODICH foreign key (ID) references NGUOIDUNG(ID),
+constraint FK_MALOAIGD_GIAODICH foreign key (MALOAIGD) references LOAIGIAODICH(MALOAIGD),
 )
 
-insert into User_info (ID, username, pass, phone, balance)
+insert into NGUOIDUNG (ID, TAIKHOAN, MATKHAU, SDT, TONGTIEN)
 values 
 ('01' , 'sondang@2004' , '123456' , '0822545545' , '1500000'),
 ('02' , 'uit@gm.edu.vn' , '678678' , '0905123123' , '2000000');
 
-insert into NganSach (ID, userID, NSname, budget, to_date)
+insert into NGANSACH (MANS, ID, TENNS, TIENNS, HSD)
 values
 ('NS01' , '01' , 'AnUong' , '800000' , '2023-12-30'),
 ('NS02' , '02' , 'MuaSam' , '1000000' , '2024-04-30');
 
-insert into Category (ID, CTname, GD_status)
+insert into LOAIGIAODICH(MALOAIGD, TENLOAIGD, TRANGTHAI)
 values
 ('CT01' , 'AnUong' , 'Payment'),
 ('CT02' , 'QuanAo' , 'Payment'),
 ('CT03' , 'Luong' , 'Income');
 
-insert into GiaoDich (ID, userID, categoryID, title, price, thumbnail, note, created_at)
+insert into GIAODICH (MAGD, ID, MALOAIGD, TENGD, TIEN, MINHHOA, GHICHU, NGAYTAO)
 values
 ('G01' , '01' , 'CT01' , 'An toi cung gia dinh' , '400000' , null , null , '2023-10-24'),
 ('G02' , '01' , 'CT03' , 'Luong thang 10' , '2000000' , null , null , '2023-10-31'),
 ('GT03', '02' , 'CT02' , 'Mua dong phuc mua dong' , '500000' , null , null , '2022-09-01');
+
