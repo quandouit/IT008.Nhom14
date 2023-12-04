ab2eec1d00ad8692ed96282c5575328f96856eaa
quandouit <22521175@gm.uit.edu.vn>
2023-12-04 22:53:56 +0700

    Add DAO, BUS (empty)

---
+++ QuanLyChiTieu/QuanLyChiTieu/Data/DAO/LoaiGiaoDichDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyChiTieu.Data.DAO
+{
+    public class LoaiGiaoDichDAO : DBConnection
+    {
+    }
+}
