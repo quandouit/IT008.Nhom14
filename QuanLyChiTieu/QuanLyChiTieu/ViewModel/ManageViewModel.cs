eb6bb639f75f37d9ec50c4a90a7ae7d0c2c1f6a1
HiamSonn <dangthanhson2016@gmail.com>
2023-12-19 14:14:16 +0700

    Add/Delete Bill

--- QuanLyChiTieu/QuanLyChiTieu/ViewModel/ManageViewModel.cs
+++ QuanLyChiTieu/QuanLyChiTieu/ViewModel/ManageViewModel.cs
@@ -5,22 +5,48 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyChiTieu.ViewModel
 {
     public class ManageViewModel : ViewModelBase
     {
         public DataTable GiaoDichData { get; set; }
-
+        public ICommand AddNewBillCommand { get; }
+        public ICommand DeleteChoosenBillCommand { get; }
+        public ICommand ShowInfoBillCommand { get; }
+        public ICommand DeleteSingleBillCommand { get; }
         public ManageViewModel()
         {
             LoadGiaoDichData();
+
+            AddNewBillCommand = new ViewModelCommand(ExecuteAddNewBillCommand);
+            DeleteChoosenBillCommand = new ViewModelCommand(ExecuteDeleteChoosenBillCommand);
+            ShowInfoBillCommand = new ViewModelCommand(ExecuteShowInfoBillCommand);
+            DeleteSingleBillCommand = new ViewModelCommand(ExecuteDeleteSingleBillCommand);
         }
 
         public void LoadGiaoDichData()
         {
             GiaoDichData = GiaoDichBUS.LietKeGiaoDich();
             OnPropertyChanged(nameof(GiaoDichData));
+        }
+        private void ExecuteAddNewBillCommand(object obj)
+        {
+            MessageBox.Show("Thuc hien Them moi giao dich");
+        }
+        private void ExecuteDeleteChoosenBillCommand(object obj)
+        {
+            MessageBox.Show("Thuc hien xoa cac giao dich da chon");
+        }
+        private void ExecuteShowInfoBillCommand(object obj)
+        {
+            MessageBox.Show("Hien thi thong tin chi tiet giao dich");
+        }
+        private void ExecuteDeleteSingleBillCommand(object obj)
+        {
+            MessageBox.Show("Thuc hien xoa giao dich nay");
         }
     }
 }
