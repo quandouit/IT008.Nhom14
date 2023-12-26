a58ad207ab05756aea696e2660829c0bbaf3bf06
Thinh_dep_trai <113797076+timothyagile@users.noreply.github.com>
2023-12-26 17:47:44 +0700

    update plan view

---
+++ QuanLyChiTieu/QuanLyChiTieu/ViewModel/AllPlanViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyChiTieu.ViewModel
+{
+    public class AllPlanViewModel : ViewModelBase
+    {
+        private ViewModelBase _planView;
+        public ViewModelBase PlanView
+        {
+            get
+            {
+                return _planView;
+            }
+
+            set
+            {
+                _planView = value;
+                OnPropertyChanged(nameof(PlanView));
+            }
+        }
+
+        private ViewModelBase _allPlanView;
+        public ViewModelBase AllPlanView
+        {
+            get
+            {
+                return _allPlanView;
+            }
+
+            set
+            {
+                _allPlanView = value;
+                OnPropertyChanged(nameof(AllPlanView));
+            }
+        }
+
+        public ICommand TurnBackCommand { get; set; }
+
+        public AllPlanViewModel()
+        {
+            TurnBackCommand = new ViewModelCommand(ExecuteTurnBackCommand);
+            //ExecuteShowPlanThisMonthCommand(null);
+        }
+
+        private void ExecuteTurnBackCommand(object obj)
+        {
+            PlanView = new PlanViewModel();
+        }
+    }
+}
